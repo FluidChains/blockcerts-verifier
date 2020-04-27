5786767da6b967fe22267dad2147a23c637299d1
Karim Boucher <boucherkarim@gmail.com>
2020-04-27 13:18:42 -0400

    Add redux devtools middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { logger } from './logger/src/';
 import { app } from '../reducers';
@@ -11,10 +11,11 @@ export function configureStore (initialState = getInitialState()) {
     // TODO: ensure the logger code does not make it to production build
     middlewares.push(logger);
   }
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
   return createStore(
     app,
     initialState,
-    applyMiddleware(...middlewares)
+    composeEnhancers(applyMiddleware(...middlewares))
   );
 }
