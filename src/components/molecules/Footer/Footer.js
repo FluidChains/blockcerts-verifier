6c7c2d7352670f19180ec0e87c3a7b5e9e5dadb3
Karim Boucher <boucherkarim@gmail.com>
2020-11-30 17:37:56 -0500

    Move to Certifico branding

--- src/components/molecules/Footer/Footer.js
+++ src/components/molecules/Footer/Footer.js
@@ -22,7 +22,7 @@ const Footer = ({ forceInPlace = false, interactive = false, theme } = {}) => {
     </section>`
     : ''
 }
-    ${BlockcertsLogo()}
+    ${BlockcertsLogo({theme: theme})}
   </footer>`;
 };
 
