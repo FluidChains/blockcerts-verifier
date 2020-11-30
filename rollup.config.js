151e47bc48613a5628c5bdbbbcc3b94ec4c549ea
Karim Boucher <boucherkarim@gmail.com>
2020-11-30 17:37:16 -0500

    Stop building for ie11

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,5 @@
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
-import cjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
 
 export default [
   {
@@ -23,41 +20,6 @@ export default [
         preferBuiltins: true
       })// ,
       // terser()
-    ]
-  },
-  {
-    input: 'src/blockcerts-verifier/index.js',
-    output: [
-      {
-        file: 'dist/ie11.js',
-        format: 'iife',
-        name: 'BlockcertsVerifier'
-      }
-    ],
-    plugins: [
-      cjs({
-        include: 'node_modules/**'
-      }),
-      babel({
-        babelrc: false,
-        presets: [['@babel/env', {
-          targets: {
-            ie: '11'
-          },
-          debug: false
-        }]],
-        plugins: [['@babel/transform-runtime', {
-          helpers: false
-        }]]
-      }),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('production')
-      }),
-      resolve({
-        browser: true,
-        preferBuiltins: true
-      }),
-      terser()
     ]
   }
 ];
